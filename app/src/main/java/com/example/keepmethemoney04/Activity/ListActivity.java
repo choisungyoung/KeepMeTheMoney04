a8357c001c1e390b5579ffc892a696cfc1214363
choisungyoung <tjddud117@naver.com>
2020-02-20 16:39:34 +0900

    숫자 -> 한글 함수 수정

--- app/src/main/java/com/example/keepmethemoney04/Activity/ListActivity.java
+++ app/src/main/java/com/example/keepmethemoney04/Activity/ListActivity.java
@@ -267,19 +267,24 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
-    public String convertHangul(String money) {
-        String[] han1 = {"", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"};
-        String[] han2 = {"", "십", "백", "천"};
-        String[] han3 = {"", "만", "억", "조", "경"};
-        StringBuffer result = new StringBuffer();
-        int len = money.length();
-        for (int i = len - 1; i >= 0; i--) {
-            result.append(han1[Integer.parseInt(money.substring(len - i - 1, len - i))]);
-            if (Integer.parseInt(money.substring(len - i - 1, len - i)) > 0)
-                result.append(han2[i % 4]);
-            if (i % 4 == 0) result.append(han3[i / 4]);
+    public String convertHangul(String value) {
+        String[] number_labels = { "", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"};
+        String[] range_labels = {"", "십", "백", "천", "만", "십만", "백만", "천만", "억", "십억"};
+
+
+        int rangeCount = value.length()-1;
+        int rawValue = Integer.valueOf(value);
+        StringBuilder result = new StringBuilder();
+        while(rangeCount >= 0) {
+            int mod = (int)Math.pow(10, rangeCount);
+            result.append(number_labels[rawValue/mod]);
+            if(rawValue/mod > 0) {
+                result.append(range_labels[rangeCount]);
+            }
+            rawValue = rawValue % mod;
+            rangeCount--;
         }
-        return result.toString();
+      return result.toString();
     }
 
 
