3973153130b6b6e56c5efc08fcb4228debb2c6a8
bomine <kbm9197@gmail.com>
2020-02-20 18:12:44 +0900

    첫화면 다만들었는데 고쳐야할 듯

--- app/src/main/java/com/example/keepmethemoney04/Activity/SplashActivity.java
+++ app/src/main/java/com/example/keepmethemoney04/Activity/SplashActivity.java
@@ -10,25 +10,54 @@ import com.example.keepmethemoney04.R;
 public class SplashActivity extends AppCompatActivity {
 
     TextView answer1, answer2;
-    TextView textView1, textView2;
+    TextView textView0, textView1, textView2, textView3, textView4;
+    TextView textView5, textView6, textView7, textView8, textView9;
+    boolean isFirst = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        textView0 = findViewById(R.id.textView0);
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+        textView6 = findViewById(R.id.textView6);
+        textView7 = findViewById(R.id.textView7);
+        textView8 = findViewById(R.id.textView8);
+        textView9 = findViewById(R.id.textView9);
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+
 
     }
 
-    public void mOnClick(View v){
-        switch (v.getId()){
-            case R.id.answer1 :
-
-                break;
-            case R.id.answer2 :
-                break;
+    public void mOnClick(View v) {
+        if (isFirst) {
+            textView5.setText("선물");
+            textView6.setText("하고 싶으신 ");
+            textView7.setText("분");
+            textView8.setText("이");
+            textView9.setText("있나요?");
+            textView0.setVisibility(View.GONE);
+            textView1.setVisibility(View.GONE);
+            textView2.setVisibility(View.GONE);
+            textView3.setVisibility(View.GONE);
+            textView4.setVisibility(View.GONE);
+            answer1.setText("네!");
+            answer2.setText("아니오.");
+            isFirst = !isFirst;
+        } else {
+            if(v == answer1){
+                finish();
+            } else {
+                moveTaskToBack(true);
+                finish();
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
         }
     }
 }
